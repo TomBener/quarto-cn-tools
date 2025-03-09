0252e46fb29f73c3e03900b1edbce21748d99c1f
TomBen <mr.ren.tao123@gmail.com>
2025-03-09 11:05:08 +0800

    Not remove spaces around citations

--- _extensions/remove-spaces/remove-spaces.lua
+++ _extensions/remove-spaces/remove-spaces.lua
@@ -1,4 +1,4 @@
---- Remove spaces before or after Chinese characters
+--- Remove spaces around Chinese characters except citations
 
 --- Copyright: © 2024–Present Tom Ben
 --- License: MIT License
@@ -8,18 +8,40 @@ local function is_non_ascii(char)
   return char and string.byte(char) > 127
 end
 
--- Process the paragraph to remove spaces adjacent to non-ASCII characters.
+-- Process the paragraph to remove spaces adjacent to non-ASCII characters
 local function process_paragraph(para)
   local cs = para.content
   local new_content = {}
 
   for i, elem in ipairs(cs) do
-    local is_space = elem.t == 'Space'
-    local next_char = cs[i + 1] and cs[i + 1].t == 'Str' and cs[i + 1].text:sub(1, 1)
-    local prev_char = cs[i - 1] and cs[i - 1].t == 'Str' and cs[i - 1].text:sub(-1)
-
-    if not (is_space and ((next_char and is_non_ascii(next_char)) or (prev_char and is_non_ascii(prev_char)))) then
+    -- If the element is not a Space, always keep it
+    if elem.t ~= 'Space' then
       table.insert(new_content, elem)
+    else
+      -- Element is a Space, determine whether to keep it
+      local next_elem = cs[i + 1]
+      local prev_elem = cs[i - 1]
+
+      -- Check adjacent characters for Chinese text
+      local next_char = next_elem and next_elem.t == 'Str' and next_elem.text:sub(1, 1)
+      local prev_char = prev_elem and prev_elem.t == 'Str' and prev_elem.text:sub(-1)
+
+      -- Check if adjacent elements are citations or citation-related elements
+      local next_is_cite = next_elem and (next_elem.t == 'Cite' or next_elem.t == 'Note')
+      local prev_is_cite = prev_elem and (prev_elem.t == 'Cite' or prev_elem.t == 'Note')
+
+      -- Determine if we need to remove this space
+      local has_adjacent_chinese = (next_char and is_non_ascii(next_char)) or (prev_char and is_non_ascii(prev_char))
+      local is_adjacent_to_cite = (prev_is_cite and next_char and is_non_ascii(next_char)) or
+          (next_is_cite and prev_char and is_non_ascii(prev_char))
+
+      -- Keep space if:
+      -- 1. It's not adjacent to Chinese characters, OR
+      -- 2. It's between a citation and Chinese characters
+      if not has_adjacent_chinese or is_adjacent_to_cite then
+        table.insert(new_content, elem)
+      end
+      -- Otherwise, remove the space (by not adding it to new_content)
     end
   end
 
@@ -29,7 +51,5 @@ end
 
 -- Return the filter for Pandoc
 return {
-  {
-    Para = process_paragraph
-  }
+  { Para = process_paragraph }
 }
